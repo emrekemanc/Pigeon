54ae9e3aff89f4dba8fb8d4a19bd69be9689ffff
Muhammet Emre Kemancı <k.emre.k25@gmail.com>
2025-05-16 18:15:31 +0300

    Additions have been made for user operations.

--- Pigeon/Data/RepositoriesImpl/AuthRepositoryImpl.swift
+++ Pigeon/Data/RepositoriesImpl/AuthRepositoryImpl.swift
@@ -5,16 +5,20 @@
 //  Created by Muhammet Emre Kemancı on 12.05.2025.
 //
 final class AuthRepositoryImpl: AuthRepository{
-    
     private let authService: AuthService = AuthService()
+    func delete(completion: @escaping (Result<Bool, any Error>) -> Void) {
+        authService.delete(completion: completion)
+    }
     
     func login(authCredentials: AuthCredentials, completion: @escaping (Result<Bool, any Error>) -> Void) {
         authService.login(authCredentials: authCredentials, completion: completion)
     }
     
-    func register(authCredentials: AuthCredentials, completion: @escaping (Result<Bool, any Error>) -> Void) {
+    func register(authCredentials: AuthCredentials, completion: @escaping (Result<String?, any Error>) -> Void) {
         authService.register(authCredentials: authCredentials, completion: completion)
     }
     
-    
+    func signOut(completion: @escaping (Result<Bool, any Error>) -> Void) {
+        authService.signOut(completion: completion)
+    }
 }
--- Pigeon/Presentation/Screens/Auth/Register/RegisterViewModel.swift
+++ Pigeon/Presentation/Screens/Auth/Register/RegisterViewModel.swift
@@ -5,7 +5,11 @@ final class RegisterViewModel{
     var onError: ((Error) -> Void)?
     
     private let registerUseCase = RegisterUserUseCases(repository: AuthRepositoryImpl())
-    func register(with authCredentials: AuthCredentials){
+    private let createUserUseCase = CreateUserUseCase(repository: UserRepositoryImpl())
+    private let deleteUseCase = DeleteUseCase(repository: AuthRepositoryImpl())
+    private let signOutUseCase = SignOutUseCase(repository: AuthRepositoryImpl())
+    
+    func register(authCredentials: AuthCredentials, userCredentials: UserCredentials){
         if let validationError = authCredentials.validationError{
             onError?(AppError.auth(validationError))
         }
@@ -13,11 +17,38 @@ final class RegisterViewModel{
         registerUseCase.execute(authCredentials: authCredentials) { [weak self] result in
             DispatchQueue.main.async {
                 switch result{
-                case .success(let result):
-                    self?.onSuccess?(result)
+                case .success(let e):
+                    var updatedCredentials = userCredentials
+                    updatedCredentials.id = e
+                    self?.createUserUseCase.execute(userCredentials: updatedCredentials) {[weak self] res in
+                        switch res{
+                        case .success(_):
+                            self?.signOutUseCase.execute { re in
+                                switch re{
+                                case .success(let r):
+                        
+                                    self?.onSuccess?(r)
+                                    
+                                case .failure(let error):
+                                    self?.onError?(error)
+                                }
+                            }
+                        case .failure(let e):
+                            self?.deleteUseCase.execute { r in
+                                switch r{
+                                case .success(_):
+                                    self?.onError?(e)
+                                    
+                                case .failure(let error):
+                                    self?.onError?(error)
+                                }
+                            }
+                        }
+                    }
                 case .failure(let error):
                     self?.onError?(error)
                 }
+                
             }
         }
     }
