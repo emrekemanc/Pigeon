37cf7c330fd2480d52451d6763f48b8ec1a00303
Muhammet Emre Kemancı <k.emre.k25@gmail.com>
2025-05-19 14:40:14 +0300

    Message Service Createing

--- Pigeon/Presentation/Screens/Main/ChatMessage/ChatMessageViewController.swift
+++ Pigeon/Presentation/Screens/Main/ChatMessage/ChatMessageViewController.swift
@@ -7,8 +7,28 @@
 
 import UIKit
 class ChatMessageViewController: UIViewController{
+    @IBOutlet weak var risiverName: UILabel!
+    @IBOutlet weak var messageListView: UITableView!
+    @IBOutlet weak var inputMessageView: CustomTextView!
+    @IBOutlet weak var sendButton: CustomButton!
+    private var messageList: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        messageListView.delegate = self
+        messageListView.dataSource = self
     }
+    @IBAction func sendButtonPress(_ sender: CustomButton) {
+    }
+}
+extension ChatMessageViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        messageList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatMessageCell", for: indexPath) as! ChatMessageCell
+        cell.cellConfigure(message: messageList[indexPath.row], selfId: <#T##String#>)
+    }
+    
+    
 }
